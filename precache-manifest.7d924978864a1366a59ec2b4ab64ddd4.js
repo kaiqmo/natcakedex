86fa23e04aa03e44ba252a159d510d33f23504e6
kaiqmo <kaiqmo@gmail.com>
2020-01-08 00:14:27 -0300

    Updates

---
+++ precache-manifest.7d924978864a1366a59ec2b4ab64ddd4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be6bcf67f10e2259f7c15e4cb0864e02",
+    "url": "/natcakedex/index.html"
+  },
+  {
+    "revision": "9b1b993d612320a13765",
+    "url": "/natcakedex/static/css/main.9fe52145.chunk.css"
+  },
+  {
+    "revision": "8f859db0807c6d8f8c2e",
+    "url": "/natcakedex/static/js/2.3e313a52.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/natcakedex/static/js/2.3e313a52.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9b1b993d612320a13765",
+    "url": "/natcakedex/static/js/main.fcdf2bb1.chunk.js"
+  },
+  {
+    "revision": "554d9faf8f9cb8795af4",
+    "url": "/natcakedex/static/js/runtime-main.75cf402a.js"
+  },
+  {
+    "revision": "9c74e9a9702bc8fdb1a20bdd98906e75",
+    "url": "/natcakedex/static/media/bolo_branco.9c74e9a9.jpg"
+  },
+  {
+    "revision": "04bd70431896f0217355a35630609887",
+    "url": "/natcakedex/static/media/bolo_branco_verm.04bd7043.jpg"
+  },
+  {
+    "revision": "a981019207e10fa4028af3a05146ec77",
+    "url": "/natcakedex/static/media/bolo_delivery.a9810192.jpg"
+  },
+  {
+    "revision": "dc9cf0e3a40d581e0c88b82313fcbf92",
+    "url": "/natcakedex/static/media/bolo_oreo.dc9cf0e3.jpg"
+  },
+  {
+    "revision": "abc489951501b2f57251a06b5fc20414",
+    "url": "/natcakedex/static/media/bolo_oreo2.abc48995.jpg"
+  },
+  {
+    "revision": "4625c9156fe0a5a90fa2cf64a6a2bdb8",
+    "url": "/natcakedex/static/media/bolo_rosa.4625c915.jpg"
+  },
+  {
+    "revision": "84fa478bd225fa00f58f6534acbdf068",
+    "url": "/natcakedex/static/media/cupcake_branco.84fa478b.jpg"
+  },
+  {
+    "revision": "a45a978039bf5da7f54b2adb0a76625b",
+    "url": "/natcakedex/static/media/cupcake_brigadeiro.a45a9780.jpg"
+  },
+  {
+    "revision": "9c0ea2c542c5c338d689521b01cb1d2f",
+    "url": "/natcakedex/static/media/tortinha_limao.9c0ea2c5.jpg"
+  }
+]);
